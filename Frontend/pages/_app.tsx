ec852a95d12bc2cc33dca9000c0993312950fc95
na0ngkim <ny3021@gmail.com>
2022-07-18 23:59:18 +0900

    프론트 초기 설정

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -1,10 +1,16 @@
-import { AppProps } from 'next/app'
-import { wrapper } from '../store';
+/* eslint-disable */
+import React, { useState, useEffect } from "react";
+import "../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import wrapper from "../store/configureStore";
+
+import { useSelector } from "react-redux";
+import { RootState } from "../store/modules";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Component {...pageProps} />
-  )
-}
+  return <Component {...pageProps} />;
+};
 
-export default wrapper.withRedux(MyApp)
+// export default MyApp;
+export default wrapper.withRedux(MyApp);
