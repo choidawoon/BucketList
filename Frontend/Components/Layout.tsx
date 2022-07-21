b4a187f6371f0a474b98d861e26121e839c015d0
na0ngkim <ny3021@gmail.com>
2022-07-21 23:59:22 +0900

    Fix: 콘텐츠 겹침 수정

---
+++ Frontend/Components/Layout.tsx
@@ -0,0 +1,18 @@
+import react, { ReactNode, FC } from "react";
+import { NextPage } from "next";
+import Navbar from "../pages/common/Navibar";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
